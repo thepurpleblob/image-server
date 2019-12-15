1abded6571074b23881469ce8e87d9b4e042e488
Howard Miller <howardsmiller@gmail.com>
2019-12-15 21:55:59 +0000

    adding some composer stuff

--- api.php
+++ api.php
@@ -1,4 +1,16 @@
 <?php
+
+require(dirname(__FILE__) . '/vendor/autoload.php');
+require(dirname(__FILE__) . '/config.php');
+
+// Database configuration
+ORM::configure('mysql:host=' . $CFG->dbhost . ';dbname=' . $CFG->dbname);
+ORM::configure('username', $CFG->dbuser);
+ORM::configure('password', $CFG->dbpass);
+
+// GUMP setup
+$gump = new GUMP();
+$_POST = $gump->sanitize($_POST);
     
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$CFG = new stdClass;
+
+$CFG->dbhost = 'localhost';
+$CFG->dbname = 'collection';
+$CFG->dbuser = 'collectionuser';
+$CFG->dbpass = 'collectionpass';
